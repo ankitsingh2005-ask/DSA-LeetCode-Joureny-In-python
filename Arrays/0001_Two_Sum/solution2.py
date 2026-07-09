932d974e8994ec1f61ad974b9e7984d72393e3d0
ankitsingh2005-ask <ankitalok2005@gmail.com>
2026-07-09 12:50:50 +0530

    Add LeetCode # 01.twoSum using two pointer approach

---
+++ Arrays/0001_Two_Sum/solution2.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    #function to return the target
+    def twoSum(self, nums, target):
+
+        nums.sort()
+
+        # two pointer to itrate on the nums
+         
+        i = 0
+        j = len(nums) -1
+
+        #while i is less then j 
+        while i < j:
+            
+            sum = nums[i] + nums[j]
+
+                #check it the i + j == target then return i and j
+            if sum == target:
+                return [i,j]
+            
+            # it the sum is less then target then increment the i to +1
+            elif sum < target:
+                i += 1
+            
+            # if the sum is greter then sum then decriment the j to -1
+            else:
+                j -= 1
+
+# Example usage 
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    nums = [2,5,7,8,12]
+    target = 9
+
+    #function calling 
+    result = solution.twoSum(nums, target)
+    print(result)
+
