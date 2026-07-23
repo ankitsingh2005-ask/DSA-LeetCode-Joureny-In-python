d8670550544d8875b1b7c2a581db5ced45e5e702
24aiml091-sketch <24aiml091@ipec.org.in>
2026-07-23 18:36:24 +0530

    Add LeetCode # 287 find the duplicate number in array using slow and fst

---
+++ Arrays/0287. Find the Duplicate Number/solution.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        slow = fast = 0
+        while True:
+
+            slow = nums[slow]
+            fast = nums[fast]
+            fast = nums[fast]
+
+            if slow == fast:
+                slow = 0
+
+                while slow != fast:
+                    slow = nums[slow]  # to find the starting point
+                    fast = nums[fast]
+
+                return slow
+
+        return 0
+
+#----------------main program------------------------
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [1,3,4,2,2]
+
+    res = solution.findDuplicate(nums)
+    print(res)
